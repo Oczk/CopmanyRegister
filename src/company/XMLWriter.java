8f84d6e6a5e3a2e1148228f162198eb9129440d9
Jakub Oczkowski <oczko.kuba@gmail.com>
2017-07-25 10:54:23 +0200

    Added saving data to xml file

---
+++ src/company/XMLWriter.java
@@ -0,0 +1,66 @@
+package company;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+
+public class XMLWriter {
+
+    public XMLWriter(CompanyData data){
+        //CompanyData data1 = data;
+
+        try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+            Document doc = docBuilder.newDocument();
+            Element rootElement = doc.createElement("Firma");
+            doc.appendChild(rootElement);
+
+            Element datas = doc.createElement("Dane");
+            rootElement.appendChild(datas);
+
+            Element regon = doc.createElement("REGON");
+            regon.appendChild(doc.createTextNode(data.getRegon()));
+            datas.appendChild(regon);
+
+            Element shortName = doc.createElement("SkroconaNazwa");
+            shortName.appendChild(doc.createTextNode(data.getShortName()));
+            datas.appendChild(shortName);
+
+            Element city = doc.createElement("Miasto");
+            city.appendChild(doc.createTextNode(data.getCity()));
+            datas.appendChild(city);
+
+            Element zipCode = doc.createElement("KodPocztowy");
+            zipCode.appendChild(doc.createTextNode(data.getZipCode()));
+            datas.appendChild(zipCode);
+
+            Element pkd = doc.createElement("PKD");
+            pkd.appendChild(doc.createTextNode(data.getPkd()));
+            datas.appendChild(pkd);
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(data.getPkd() + ".xml"));
+
+            transformer.transform(source, result);
+            System.out.println("file saved");
+
+
+        } catch (ParserConfigurationException | TransformerException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
